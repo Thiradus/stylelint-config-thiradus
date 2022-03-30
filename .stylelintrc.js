85808ba13e59ebcfe4074d2f58f8a7efc34fb27e
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2022-03-29 22:16:36 -0400

    Initial Commit
    Signed-off-by: Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"extends": "stylelint-config-standard",
+	"rules": {
+		"color-hex-case": "upper",
+		"color-hex-length": "long",
+		"indentation": "tab",
+		"no-descending-specificity": null
+	}
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+const stylelint = require('./.stylelint.js');
+
+module.exports = stylelint;
